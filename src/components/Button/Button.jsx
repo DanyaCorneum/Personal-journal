dce44406559895c89ef151d3d8d1db6ec336a443
Ratkov_Daniil <daniilratkov3@gmail.com>
2025-02-26 12:33:26 +0700

    added componets Button and JournalItem

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import "./Button.css";
+
+function Button() {
+  return <button className="button save">Сохранить</button>;
+}
+
+export default Button;
---
+++ src/components/JournalItem/JournalItelm.jsx
@@ -0,0 +1,15 @@
+import "./JournalItem.css";
+
+function JournalItem() {
+  return (
+    <div className="journal-item">
+      <h2 className="journal-item__header">1</h2>
+      <p className="journal-item__inner">
+        <div className="journal-item__date">2</div>
+        <div className="journal-item__text">3</div>
+      </p>
+    </div>
+  );
+}
+
+export default JournalItem;
